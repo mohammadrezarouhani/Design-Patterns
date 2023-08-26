115f2279b4669c79f0d12d812817a869baa8bf05
mohammadreza rouhani <rezarouhanitonekaboni@gmail.com>
2023-08-26 07:48:28 +0330

    diagram added

--- 1-Memento.py
+++ 1-Memento.py
@@ -14,14 +14,6 @@ class Editor:
         self._font: str = ''
         self._font_size: int = 0
 
-    def set_content(self, content: str, font: str = 'default', font_size: int = 12):
-        self.content = content
-        self.font = font
-        self.font_size = font_size
-
-    def get_content(self) -> tuple:
-        return (self.content, self.font, self.font_size)
-
     def create_state(self) -> EditorState:
         return EditorState(self.content, self.font, self.font_size)
 
@@ -29,6 +21,14 @@ class Editor:
         self.content = state.content
         self.font = state.font
         self.font_size = self.font_size
+
+    def set_content(self, content: str, font: str = 'default', font_size: int = 12):
+        self.content = content
+        self.font = font
+        self.font_size = font_size
+
+    def get_content(self) -> tuple:
+        return (self.content, self.font, self.font_size)
 
 
 class EditorHistory:
@@ -38,7 +38,7 @@ class EditorHistory:
     def pushHistory(self, state: EditorState):
         self.state.append(state)
 
-    def restorState(self) -> EditorState:
+    def popHistory(self) -> EditorState:
         last_state = self.state.pop()
         return last_state
 
@@ -54,5 +54,5 @@ if __name__ == '__main__':
     history.pushHistory(editor.create_state())
 
     editor.set_content('third content')
-    editor.restore_state(history.restorState())
+    editor.restore_state(history.popHistory())
     print(editor.get_content())
